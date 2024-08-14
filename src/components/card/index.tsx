6de6f30c91609066a994a5cd5584331f4071fb70
GeorgiaCarin <georgia.carin@simplesmeiosdepagamento.com.br>
2024-08-13 22:03:37 -0300

    Tele de Vantagens

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -4,16 +4,17 @@ type Props = {
     title: string,
     text:string
     type: string, 
+    img: string
 
 } 
 
-export const Card = ({title,text,type}: Props) => {
+export const Card = ({title,text,type,img}: Props) => {
 
     return (
         <div className={`${type}`}>
-            <img className='' src={seguranca} alt="" />
-            <div className= ' text-left p-6'>
-                <div className=' capitalize text-black text-2xl text-bb-lyellow font-extrabold  pb-2' >{title}</div>
+            <img className='' src={img} alt="" />
+            <div className= ' text-left p-6 leading-4 '>
+                <div className='capitalize text-black text-2xl leading-6 text-bb-lyellow font-extrabold  pb-2' >{title}</div>
                 <div className='font-normal text-black text-[15px] '>{text}</div>
             </div>
   
--- src/sections/totem-vertical/step_two/index.tsx
+++ src/sections/totem-vertical/step_two/index.tsx
@@ -1,33 +1,72 @@
 
 import { Card } from '../../../components/card'
-import arrow from '../../../assets/arrow.svg'
 import '../../../index.css'
-
-
+import seguranca from '../../../assets/segurança.png'
+import ondeVoceEstiver from '../../../assets/ondeVoceEstiver.png'
+import semCustos from '../../../assets/semCustos.png'
+import cashback from '../../../assets/CashBack.png'
+import shopping from '../../../assets/shoppingBB.png'
+import facilidade from '../../../assets/Facilidade.png'
 type props = {
     setSection : React.Dispatch<React.SetStateAction<number>>
 //     section: number
 }
+type dataProps = {
+    title: string,
+    text: string,
+    img_url: string
 
+}
+
+const data : dataProps[] = [
+    {
+        title: 'Segurança',
+        text: 'Os seus dados e as suas transações estão sempre em um ambiente seguro, protegido pelo BB.',
+        img_url: seguranca,
+    },
+    {
+        title: 'Onde você estiver',
+        text: 'Online no App BB, internet banking ou WhatsApp. Pelo telefone com o SAC e a Central de Relacionamento BB. Além das agência, caixas eletrônicos ou Correspondentes Mais BB.',
+        img_url: ondeVoceEstiver,
+    },
+    {
+        title: 'Sem custos',
+        text: 'Online no App BB, internet banking ou WhatsApp. Pelo telefone com o SAC e a Central de Relacionamento BB. Além das agência, caixas eletrônicos ou Correspondentes Mais BB.',
+        img_url: semCustos,
+    },
+    {
+        title: 'Cashback, investimento ou pontos',
+        text: 'Mais liberdade pra escolher o que usar e como usar.',
+        img_url: cashback,
+    },
+    {
+        title: 'Shopping BB',
+        text: 'Cupons, Gift Cards e diversos produtos de marcas renomadas, com descontos e cashback.',
+        img_url: shopping,
+    },
+    {
+        title: 'Facilidade pra você',
+        text: 'Pix gratuito, débitos e agendamentos automáticos, saldo em conta remunerado, plataforma de investimentos e o que mais precisar.',
+        img_url: facilidade,
+    },
+]
 export const StepTwo = ({setSection}:props) => {
     
     return (
-        <div className=' flex flex-col h-screen w-screen bg-vertical min-h-screen bg-cover justify-between'>
+        <div className=' flex flex-col w-full  bg-vertical h-full bg-cover justify-between'>
             <div className= 'flex w-full flex-col gap-5 mt-24 text-center items-center'>
                 <div>
                     <h1 className='font-black text-6xl text-bb-blue p-10  capitalize'>Vantagens para você</h1>
                     <p className='font-normal text-3xl w-[763px]'>Abra sua conta corrente completa, totalmente digital, e simplifique sua vida financeira.</p>
-
                 </div>
                 <div className='inline-grid grid-cols-3 gap-6 '>
-                   
-                        <Card title='Segurança' text='Os seus dados e as suas transações estão sempre em um ambiente seguro, protegido pelo BB.' icon='icon' type='card ' />
-                        
-
+                        {data.map((item) => (
+                            <div>
+                                <Card title={item.title} text={item.text} type='card' img={item.img_url}/>
+                            </div>
+                        ))}
                 </div>
-
             </div>
-            
         </div>
     )
 }
